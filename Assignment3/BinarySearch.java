641bc58b3f0fd744beede246badd25e6362addbd
Tyler Vultaggio <tyler.vultaggio1@marist.edu>
2021-11-05 11:04:25 -0400

    My Searches

--- Assignment3/BinarySearch.java
+++ Assignment3/BinarySearch.java
@@ -1,19 +1,66 @@
-/**
- * 
- */
+import java.io.FileNotFoundException;
 
-/**
- * @author kidty
- *
- */
-public class BinarySearch {
+/** @author Tyler Vultaggio
+* Assignment 3
+* Due Friday 11/5/2021
+* Algorithms
+*/
+public class BinarySearch 
+{
+	
+	public static int binSearch(String sortedArray[], String target)
+	{
+		 int start = 0;
+		 int end = sortedArray.length - 1;
+		 int comparisons = 0;
+	        while (start <= end) 
+	        {
+	        	comparisons++;
+	        	//System.out.println(comparisons);
+	            int position = start + (end - start) / 2;
+	 
+	            int res = target.compareTo(sortedArray[position]);
+	 
+	            // Check if x is present at mid
+	            if (res == 0)
+	                return comparisons;
+	 
+	            // If x greater, ignore left half
+	            if (res > 0)
+	            	start = position + 1;
+	 
+	            // If x is smaller, ignore right half
+	            else
+	            	end = position - 1;
+	        }
+	 
+	        return -1;
+	}
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public static void main(String[] args) throws FileNotFoundException 
+	{
+		int totalCompares = 0;
+		int averageCompares = 0;
+		String[] TempArray = new String[666];
+		TempArray = myFileReader.fileArray();
+		String[] TargetArray = new String[42];
+		for(int i = 0; i < TargetArray.length; i++)
+		{
+			TargetArray[i] = TempArray[i];
+		}
+		
+		TempArray = MergeSort.mSort(TempArray, 0, TempArray.length-1);
+		
+		for(int i = 0; i < TargetArray.length; i++)
+		{
+			totalCompares = totalCompares + binSearch(TempArray, TargetArray[i]);
+		}
+		
+		averageCompares = totalCompares/TargetArray.length;
+		System.out.println("This is the number of total comparisons: " + totalCompares);
+		System.out.println("This is the average number of comparisons: " + averageCompares);
+		
 	}
 
 }
--- Assignment3/LinearSearch.java
+++ Assignment3/LinearSearch.java
@@ -1,19 +1,63 @@
-/**
- * 
- */
+import java.io.FileNotFoundException;
 
-/**
- * @author kidty
- *
- */
-public class LinearSearch {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+/** @author Tyler Vultaggio
+* Assignment 3
+* Due Friday 11/5/2021
+* Algorithms
+*/
+public class LinearSearch 
+{
+	public static int linSearch(String SortedItems[], String target)
+	{
+		int comparisons = 0;
+		
+		for(int i = 0; i < SortedItems.length; i++)
+		{
+			comparisons++;
+			if(SortedItems[i].equalsIgnoreCase(target))
+			{
+				//System.out.println(comparisons);
+				return comparisons;
+			}
+			
+		}
+		return comparisons;
+	}
+	
+	
+	public static void printArray(String[] printableArray)
+	{
+		for(int i = 0; i < 	printableArray.length; i++)
+		{
+			System.out.println(	printableArray[i]);
+		}
+	}
+	
+	public static void main(String[] args) throws FileNotFoundException 
+	{
+		int totalCompares = 0;
+		int averageCompares = 0;
+		String[] TempArray = new String[666];
+		TempArray = myFileReader.fileArray();
+		String[] TargetArray = new String[42];
+		for(int i = 0; i < TargetArray.length; i++)
+		{
+			TargetArray[i] = TempArray[i];
+		}
+		
+		//printArray(TargetArray);
+		TempArray = MergeSort.mSort(TempArray, 0, TempArray.length-1);
+		
+		for(int i = 0; i < TargetArray.length; i++)
+		{
+			totalCompares = totalCompares + linSearch(TempArray, TargetArray[i]);
+		}
+		
+		averageCompares = totalCompares/TargetArray.length;
+		System.out.println("This is the number of total comparisons: " + totalCompares);
+		System.out.println("This is the average number of comparisons: " + averageCompares);
+		
+		
 	}
 
 }
